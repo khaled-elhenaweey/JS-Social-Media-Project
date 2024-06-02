b6953d4f3a6e53c41eceeafdfbe13b2e225902b3
khaled El-henaweey <71648492+khaled-elhenaweey@users.noreply.github.com>
2024-06-02 14:40:49 +0300

    add axios and bootstrap icons

--- assets/js/home.js
+++ assets/js/home.js
@@ -8,24 +8,21 @@ function getAllPosts() {
       let posts = response.data.data;
       for (const post of posts) {
         let content = `
-          <div class="card mb-3">
-          <div class="card-header">
-    
-              <img src="${post.author.profile_image}" class="rounded-circle" alt="card-img" style='min-width: 40px; max-width: 40px; max-height: 40px'>
-           <span class="fw-bold">@${post.author.name}</span>
-          </div>
-          <div class="card-body">
-              <img src="            ${post.image}
-              " class="w-100">
+        <div class="card mb-3">
+        <div class="card-header">
+            <img src="${post.author.profile_image}" class="rounded-circle" alt="card-img" style='min-width: 40px; max-width: 40px; max-height: 40px'>
+        <span class="fw-bold">@${post.author.name}</span>
+        </div>
+        <div class="card-body">
+            <img src="            ${post.image}
+            " class="w-100">
             <h5 class="card-title">
             ${post.created_at}
             </h5>
             <p class="card-text">  ${post.body}</p>
-          </div>
         </div>
-          
-          
-          `;
+        </div>
+        `;
         console.log(post);
         postsBlock.innerHTML += content;
       }
